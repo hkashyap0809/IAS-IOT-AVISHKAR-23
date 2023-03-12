b2514bb6fcf828e90fa233396da73421fbf1a4f2
yash-pathak-1997 <yashhpathakk1997@gmail.com>
2023-03-12 15:32:01 +0530

    Added health check function

---
+++ NodeManager/healthCheck.py
@@ -0,0 +1,44 @@
+import json
+from azure.identity import ClientSecretCredential
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.compute.models import InstanceViewTypes, InstanceViewStatus
+import psutil
+
+
+def get_health_vm(vm_name="VM1"):
+
+    with open('../Resources/Config/nodes_config.json', 'r') as f:
+        res_json = json.load(f)
+
+    # Create a ClientSecretCredential instance
+    credential = ClientSecretCredential(res_json["tenant_id"], res_json["client_id"], res_json["client_secret"])
+
+    # Create a ComputeManagementClient instance
+    compute_client = ComputeManagementClient(credential, res_json["subscription_id"])
+
+    # Get the details of the VM
+    vm = compute_client.virtual_machines.get(res_json["resource_group_name"], vm_name)
+
+    # Get the CPU usage of the VM using psutil
+    cpu_usage_percent = psutil.cpu_percent()
+
+    # Get the memory usage of the VM using psutil
+    memory_usage_percent = psutil.virtual_memory().percent
+
+    # Get the disk usage of the VM using the Azure Compute Management Client
+    disk_usage_percent = 0
+    for disk in vm.storage_profile.data_disks:
+        disk_instance_view = compute_client.disks.get(res_json["resource_group_name"], disk.name,
+                                                      expand=InstanceViewTypes.instance_view).instance_view
+        disk_status = [status for status in disk_instance_view.statuses if status.code.startswith('PowerState/')][0]
+        if disk_status.display_status == 'Attached':
+            for volume in disk_instance_view.volumes:
+                if volume.status == InstanceViewStatus('Attached'):
+                    disk_usage_percent += volume.disk_usage_percent
+
+    # Print the resource utilization of the VM
+    print(f"CPU usage: {cpu_usage_percent}%")
+    print(f"Memory usage: {memory_usage_percent}%")
+    print(f"Disk usage: {disk_usage_percent}%")
+
+
--- NodeManager/main.py
+++ NodeManager/main.py
@@ -1,2 +1,5 @@
+from healthCheck import get_health_vm
+
 if __name__ == "__main__":
+    get_health_vm()
     print("Hi, this is NodeManager")
