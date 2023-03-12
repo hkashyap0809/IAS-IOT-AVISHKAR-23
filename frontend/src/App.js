669a52bdda90067dce74dac4ab9e32e5a47351b9
Prashant Kumar <prashantkumarr81@gmail.com>
2023-03-12 16:35:16 +0530

    login and signup page

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import "./signup.css";
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        {" "}
+        <h1 className="head"> AVISHKAR </h1>{" "}
+      </center>
+      <div class="center">
+        <h1>Login</h1>
+        <form method="post">
+          <div class="txt_field">
+            <input type="text" required />
+            <span></span>
+            <label>Username</label>
+          </div>
+          <div class="txt_field">
+            <input type="password" required />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <div class="pass">Forgot Password?</div>
+          <input type="submit" value="Login" />
+          <div class="signup_link">
+            Not a member? <a href="#">Signup</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
