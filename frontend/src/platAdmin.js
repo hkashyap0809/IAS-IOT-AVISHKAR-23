aa15c0eb3460b7ce1d9b24053e06135b284a0d1a
Rishabh Gupta <dalbazarrishabhgupta@gmail.com>
2023-04-08 16:08:23 +0530

    platAdmin

---
+++ frontend/src/platAdmin.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+function Platformadmin() {
+  return <div className="App"></div>;
+}
+
+export default SignUp;
