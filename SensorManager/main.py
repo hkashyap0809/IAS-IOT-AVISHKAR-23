8531a72552be91a3c32bdd941d4b1e7f3533ea52
yash-pathak-1997 <yashhpathakk1997@gmail.com>
2023-04-21 12:10:30 +0530

    Formatted

--- SensorManager/main.py
+++ SensorManager/main.py
@@ -1,12 +1,12 @@
 import sys
 import os
-from flask import Flask,request
+from flask import Flask, request
 import json
 
 from flask_cors import cross_origin
 
-from kafka_consumer_sensor import get_latest_node_data,get_latest_n_node_data
-from json_utilities import read_JSON,FOLDER_PATH
+from kafka_consumer_sensor import get_latest_node_data, get_latest_n_node_data
+from json_utilities import read_JSON, FOLDER_PATH
 
 # Get the absolute path of the directory containing this script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -16,14 +16,14 @@ print(os.getcwd())
 
 app = Flask(__name__)
 
-nodes = read_JSON(FOLDER_PATH,'nodes.json')
-sensors = read_JSON(FOLDER_PATH,'unique_sensors.json')
-verticals = read_JSON(FOLDER_PATH,'verticals.json')
-vertical_partition = read_JSON(FOLDER_PATH,'verticals.json')
+nodes = read_JSON(FOLDER_PATH, 'nodes.json')
+sensors = read_JSON(FOLDER_PATH, 'unique_sensors.json')
+verticals = read_JSON(FOLDER_PATH, 'verticals.json')
+vertical_partition = read_JSON(FOLDER_PATH, 'verticals.json')
 
 
 # SENSOR REGISTRY APIs
-@app.route('/api/sensor/register/vertical',methods=['POST'])
+@app.route('/api/sensor/register/vertical', methods=['POST'])
 def vertical_registration():
     req_body = request.get_json()
     vertical = req_body['vertical']
@@ -31,6 +31,7 @@ def vertical_registration():
     descriptor = req_body['descriptor']
     print(descriptor)
     return "vertical registration"
+
 
 # @app.route('/api/sensor/register/node',methods=['POST'])
 # def node_registration():
@@ -41,35 +42,36 @@ def vertical_registration():
 #     return "sensor registration"
 
 # SENSOR DATA APIs
-@app.route('/api/sensor/data/latest/<nodename>',methods=['GET'])
+@app.route('/api/sensor/data/latest/<nodename>', methods=['GET'])
 def node_data(nodename):
-    try :
+    try:
         if nodename in nodes:
             n_last = request.args.get('last')
             if n_last:
                 latest_data = get_latest_n_node_data()
-                return json.dumps({'statusCode':'200','message':'last n data'})
+                return json.dumps({'statusCode': '200', 'message': 'last n data'})
             else:
                 latest_data = get_latest_node_data()
-                return json.dumps({'statusCode':'200','message':'last data'})
+                return json.dumps({'statusCode': '200', 'message': 'last data'})
         else:
-            return json.dumps({'statusCode':'400','message':'error'})
+            return json.dumps({'statusCode': '400', 'message': 'error'})
     except:
-        return json.dumps({'statusCode':'400','message':'error'})
+        return json.dumps({'statusCode': '400', 'message': 'error'})
 
-@app.route('/api/sensor/data/latest/<nodename>/<sensor>',methods=['GET'])
-def sensor_node_data(nodename,sensor):
-    try :
+
+@app.route('/api/sensor/data/latest/<nodename>/<sensor>', methods=['GET'])
+def sensor_node_data(nodename, sensor):
+    try:
         if nodename in nodes and sensor in sensors:
             n_last = request.args.get('last')
             if n_last:
-                return json.dumps({'statusCode':'200','message':'last n data'})
+                return json.dumps({'statusCode': '200', 'message': 'last n data'})
             else:
-                return json.dumps({'statusCode':'200','message':'last data'})
+                return json.dumps({'statusCode': '200', 'message': 'last data'})
         else:
-            return json.dumps({'statusCode':'400','message':'error'})
+            return json.dumps({'statusCode': '400', 'message': 'error'})
     except:
-        return json.dumps({'statusCode':'400','message':'error'})
+        return json.dumps({'statusCode': '400', 'message': 'error'})
 
 
 @app.route("/home", methods=['GET'])
--- SensorManagerOld/dataSend.py
+++ SensorManagerOld/dataSend.py
@@ -25,9 +25,11 @@ HEADERS = {
 }
 PAYLOAD = {}
 
+
 # printing readable JSON
 def print_JSON(json_dict):
-    print(json.dumps(json_dict,indent=4))
+    print(json.dumps(json_dict, indent=4))
+
 
 # find descriptor
 def find_descriptor(url_des):
@@ -36,21 +38,23 @@ def find_descriptor(url_des):
     data = json.loads(response.text)
     return data["m2m:cin"]["con"]
 
+
 # serializer for kafka
 def json_serializer(sensor_data):
     return json.dumps(sensor_data).encode('utf-8')
 
+
 # getting partition and topic details
 def give_partition_and_topic_details(descriptor):
-    partition_details={}
-    topic_details={}
-    sensor_types=[]
-    discard_values=['timestamp','occupancy-state']
+    partition_details = {}
+    topic_details = {}
+    sensor_types = []
+    discard_values = ['timestamp', 'occupancy-state']
     partition_number = 0
 
     for key in descriptor:
         partition_details[key] = partition_number
-        partition_number = partition_number+1
+        partition_number = partition_number + 1
         for sensor_type in descriptor[key]:
             sensor_types.append(sensor_type)
 
@@ -59,10 +63,10 @@ def give_partition_and_topic_details(descriptor):
         sensor_types.remove(value)
 
     for sensor_type in sensor_types:
-        topic_details[sensor_type]=f"{sensor_type}-topic"
+        topic_details[sensor_type] = f"{sensor_type}-topic"
 
-    return partition_details,topic_details
-    
+    return partition_details, topic_details
+
 
 # get the descriptor details
 def get_descriptor():
@@ -71,15 +75,15 @@ def get_descriptor():
         'Accept': 'application/json'
     }
     PAYLOAD = {}
-    response = requests.request("GET",URL,headers=HEADERS,data=PAYLOAD)
+    response = requests.request("GET", URL, headers=HEADERS, data=PAYLOAD)
     data = json.loads(response.text)
-    descriptor ={}
+    descriptor = {}
 
     for itr in data["m2m:ae"]["m2m:cnt"]:
         description = find_descriptor(URL_DESC + "/" + itr["rn"])
         descriptor[itr["rn"]] = ast.literal_eval(description)
 
-    return data,descriptor
+    return data, descriptor
 
 
 # get request to getch the latest data
@@ -89,7 +93,8 @@ def fetch_latest_data(url_des):
     data = json.loads(response.text)
     return data["m2m:cin"]["con"]
 
-def fetch_data_write_in_topic(url_desc,itr,descriptor):
+
+def fetch_data_write_in_topic(url_desc, itr, descriptor):
     sensor_data = fetch_latest_data(URL_DESC + "/" + itr["rn"])
     nodename = itr["rn"]
     node_descriptor = descriptor[itr["rn"]]
@@ -99,7 +104,7 @@ def fetch_data_write_in_topic(url_desc,itr,descriptor):
     time_stamp = sensor_data[0]
     occupancy_state = sensor_data[1]
 
-    for i in range(2,len(parameters)):
+    for i in range(2, len(parameters)):
         if (parameters[i] not in data_dict):
             data_dict[parameters[i]] = []
         node_data = {
@@ -111,30 +116,27 @@ def fetch_data_write_in_topic(url_desc,itr,descriptor):
         data_dict[parameters[i]].append(node_data)
 
 
-
 if __name__ == "__main__":
     print("URL ", URL)
     print("URL DESC", URL_DESC)
 
-    data,descriptor = get_descriptor()
+    data, descriptor = get_descriptor()
     print("DESCRIPTOR DICT ")
     print_JSON(descriptor)
 
-    partition_details,topic_details = give_partition_and_topic_details(descriptor)
+    partition_details, topic_details = give_partition_and_topic_details(descriptor)
     print("PARTITION DETAILS")
     print_JSON(partition_details)
     print("TOPIC DETAILS")
     print_JSON(topic_details)
     n_partition = len(partition_details)
 
-    
-    
     while True:
-        data_dict={}
+        data_dict = {}
         threads = []
-        #looping through nodes 
+        # looping through nodes
         for itr in data["m2m:ae"]["m2m:cnt"]:
-            thread = threading.Thread(target=fetch_data_write_in_topic, args=(URL_DESC,itr,descriptor))
+            thread = threading.Thread(target=fetch_data_write_in_topic, args=(URL_DESC, itr, descriptor))
             threads.append(thread)
             thread.start()
 
@@ -149,11 +151,11 @@ if __name__ == "__main__":
         for sensor in data_dict:
             for sensor_data in data_dict[sensor]:
                 kafka_topic = topic_details[sensor]
-                print(f"topic_name={kafka_topic},partition={partition_details[sensor_data['nodename']]},data={sensor_data}")
-                producer = KafkaProducer(bootstrap_servers=['20.196.205.46:9092'],value_serializer=json_serializer)
-                producer.send(kafka_topic,value=sensor_data,partition=partition_details[sensor_data['nodename']])
+                print(
+                    f"topic_name={kafka_topic},partition={partition_details[sensor_data['nodename']]},data={sensor_data}")
+                producer = KafkaProducer(bootstrap_servers=['20.196.205.46:9092'], value_serializer=json_serializer)
+                producer.send(kafka_topic, value=sensor_data, partition=partition_details[sensor_data['nodename']])
                 producer.close()
-        
 
 #     # give the data to the producer
 #     # producer.send("sensor-kafka-topic",value=nodes_json)
@@ -161,7 +163,6 @@ if __name__ == "__main__":
 #     break
 #     # time.sleep(10)
 #     # result = thread.join()
-
 
 
 # def main_fun( URL_DESC, itr ):
@@ -204,8 +205,6 @@ if __name__ == "__main__":
 # print("DESCRIPTOR DICT", json.dumps(desc_dict,indent=4))
 
 
-
-
 # fetching data from OM2M using Threading
 # producer = KafkaProducer(bootstrap_servers=['20.196.205.46:9092'],value_serializer=json_serializer)
 
@@ -240,4 +239,3 @@ if __name__ == "__main__":
 #     break
 #     # time.sleep(10)
 #     # result = thread.join()
-
